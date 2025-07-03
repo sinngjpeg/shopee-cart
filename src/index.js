9da12b9d1da43b50a299c22e46cb3394ea5fdc6c
Ingrid Silva <ingridsilvatec@outlook.com>
2025-07-03 03:02:54 -0300

    delete item

--- src/index.js
+++ src/index.js
@@ -11,7 +11,10 @@ const item1 = await createItem("Shoes", 50.99, 1);
 const item2 = await createItem("Shirt", 20.00, 3);
 
 await cartService.addItem(myCart, item1);
-await cartService.addItem(myWishList, item2);
+await cartService.addItem(myCart, item2);
+
+await cartService.deleteItem(myCart, item1.name);
+await cartService.deleteItem(myCart, item2.name);
 
 console.log("Shoppe Cart Total:")
 await cartService.calculateTotal(myCart);
--- src/services/cart.js
+++ src/services/cart.js
@@ -1,15 +1,21 @@
 
 
-async function addItem(userCart, item){
+async function addItem(userCart, item) {
     userCart.push(item);
     return userCart;
 }
 
-async function deleteItem(userCart, name){}
-async function removeItem(userCart, index){}
+async function deleteItem(userCart, name) {
+    const index = userCart.findIndex(item => item.name === name);
+    if (index !== -1) {
+        userCart.splice(index, 1);
+    }
+}
 
-async function calculateTotal(userCart){
-    const result =  userCart.reduce((total, item) => total + item.subtotal(), 0);
+async function removeItem(userCart, index) { }
+
+async function calculateTotal(userCart) {
+    const result = userCart.reduce((total, item) => total + item.subtotal(), 0);
     console.log(result)
 }
 
